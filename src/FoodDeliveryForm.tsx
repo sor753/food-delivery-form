351bd92156156043a183e03aaca6c3df034c5879
show kawazoe <show753@icloud.com>
2026-01-02 09:44:41 +0900

    バリデーション設定

--- src/FoodDeliveryForm.tsx
+++ src/FoodDeliveryForm.tsx
@@ -1,12 +1,28 @@
 import { useForm, type FieldErrors } from 'react-hook-form';
 
 type FoodDeliveryFormType = {
+  orderNo: string;
   customerName: string;
   mobile: string;
+  email: string;
 };
 
 const FoodDeliveryForm = () => {
-  const { register, handleSubmit } = useForm<FoodDeliveryFormType>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FoodDeliveryFormType>({
+    mode: 'all',
+    delayError: 300,
+    // reValidateMode: 'onChange',
+    defaultValues: {
+      orderNo: new Date().valueOf().toString(),
+      customerName: '',
+      mobile: '',
+      email: '',
+    },
+  });
 
   const onSubmit = (formData: FoodDeliveryFormType) => {
     console.log('Form data:', formData);
@@ -17,27 +33,105 @@ const FoodDeliveryForm = () => {
   };
 
   return (
-    <form autoComplete="off" onSubmit={handleSubmit(onSubmit, onError)}>
-      <div className="form-floating mb-3">
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Customer Name"
-          {...register('customerName', {
-            required: 'Customer Name is required',
-          })}
-        />
-        <label htmlFor="">Customer Name</label>
+    <form
+      autoComplete="off"
+      noValidate
+      onSubmit={handleSubmit(onSubmit, onError)}
+    >
+      <div className="row mb-2">
+        <div className="col">
+          <div className="form-floating mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="#Order No"
+              {...register('orderNo', {
+                required: 'Order No is required',
+              })}
+            />
+            <label htmlFor="">#Order No</label>
+          </div>
+        </div>
+        <div className="col">
+          <div className="form-floating mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Mobile"
+              {...register('mobile', {
+                minLength: {
+                  value: 10,
+                  message: 'Mobile number must be at least 10 digits',
+                },
+                maxLength: {
+                  value: 10,
+                  message: 'Mobile number must be at most 10 digits',
+                },
+                required: 'Mobile number is required',
+              })}
+            />
+            <label htmlFor="">Mobile</label>
+            {errors.mobile && (
+              <div className="error-feedback">{errors.mobile?.message}</div>
+            )}
+          </div>
+        </div>
       </div>
-      <div className="form-floating mb-3">
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Mobile"
-          {...register('mobile', { required: 'Mobile is required' })}
-        />
-        <label htmlFor="">Mobile</label>
+      <div className="row mb-2">
+        <div className="col">
+          <div className="form-floating mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Customer Name"
+              {...register('customerName', {
+                required: 'Customer Name is required',
+              })}
+            />
+            <label htmlFor="">Customer Name</label>
+            {errors.customerName && (
+              <div className="error-feedback">
+                {errors.customerName?.message}
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="col">
+          <div className="form-floating mb-3">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Email"
+              {...register('email', {
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: 'Invalid email address',
+                },
+                validate: {
+                  notFake: (value) => {
+                    return (
+                      value != 'email@gmail.com' ||
+                      'Pasrticualr email is not allowed'
+                    );
+                  },
+                  notFromBlackListedDomain: (value) => {
+                    return (
+                      (!value.endsWith('xyz.com') &&
+                        !value.endsWith('example.com')) ||
+                      'This domain is not allowed'
+                    );
+                  },
+                },
+              })}
+            />
+            <label htmlFor="">Email</label>
+            {errors.email && (
+              <div className="error-feedback">{errors.email?.message}</div>
+            )}
+          </div>
+        </div>
       </div>
+
       <button type="submit" className="btn btn-primary">
         Submit
       </button>
